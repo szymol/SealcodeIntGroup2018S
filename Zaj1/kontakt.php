56212cb264a7620a1364217d2b98a037d95c7166
Aleksander Szymczak <aleszy10@st.amu.edu.pl>
2018-03-19 23:35:44 +0100

    taktyczny

---
+++ Zaj1/kontakt.php
@@ -0,0 +1,37 @@
+<?php
+
+// Tworzymy zmienną dla imienia i nazwiska
+$name = $_POST['name'];
+
+// Tworzymy zmienną dla adresu email
+$email = $_POST['email'];
+
+// Tworzymy zmienną dla wiadomości
+$message = $_POST['message'];
+
+// Podajesz adres email z którego ma być wysłana wiadomość
+$odkogo = "twojadres@gmail.com";
+
+// Podajesz adres email na który chcesz otrzymać wiadomość
+$dokogo = "twojadres@gmail.com";
+
+// Podajesz tytuł jaki ma mieć ta wiadomość email
+$tytul = "Wiadomość z pracy domowej";
+
+// Przygotowujesz treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imie i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Wysyłamy wiadomość
+$sukces = mail($dokogo, $tytul, $wiadomosc, "Od: <$odkogo>");
+
+// Przekierowywujemy na potwierdzenie
+if ($sukces){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+?>
